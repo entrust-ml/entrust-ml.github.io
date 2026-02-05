3097f3dd1c93c37adb3db603967c7cb5ca59eb12
gietema <jochem@giete.ma>
2026-02-05 13:09:56 +0000

    Update work with us section

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -48,9 +48,10 @@ export default function CareersPage() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
               </svg>
             </div>
-            <h3 className="font-semibold text-apple-gray-700 mb-2">Impactful Research</h3>
+            <h3 className="font-semibold text-apple-gray-700 mb-2">Real-world Impact</h3>
             <p className="text-sm text-apple-gray-500">
-              Work on problems related to real-world applications, bias mitigation and fairness.
+              Work on problems related to real-world applications.
+              Our product is used by millions of people worldwide to verify their identity.
             </p>
           </div>
           <div className="text-center">
@@ -59,9 +60,9 @@ export default function CareersPage() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
               </svg>
             </div>
-            <h3 className="font-semibold text-apple-gray-700 mb-2">World-Class Team</h3>
+            <h3 className="font-semibold text-apple-gray-700 mb-2">Relevant problems</h3>
             <p className="text-sm text-apple-gray-500">
-              Collaborate with researchers who are leaders in their fields.
+              Work on deepfake detection, bias mitigation, document understanding and other problems that are important to our customers and society.
             </p>
           </div>
           <div className="text-center">
@@ -72,7 +73,7 @@ export default function CareersPage() {
             </div>
             <h3 className="font-semibold text-apple-gray-700 mb-2">Great Environment</h3>
             <p className="text-sm text-apple-gray-500">
-              Access to cutting-edge compute and resources to support your research.
+              We work with cutting-edge ML tooling and infrastructure, and have a culture of learning and growth.
             </p>
           </div>
         </div>
